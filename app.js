dce695794be76bac9062797b77713b6be2c90dc3
Vishal559 <>
2021-01-17 23:26:21 +0530

    Final Commit

--- app.js
+++ app.js
@@ -1,43 +1,51 @@
-
 require('dotenv').config();
 const express = require('express');
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const User = require('./models/userSignup');
-const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+// const session = require('express-session');
+// const passport = require('passport');
+// const LocalStrategy = require('passport-local').Strategy;
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-const userRouter = require("./routes/userSignup");
-const mailgun = require("mailgun-js");
-const DOMAIN = 'sandbox1062316fb8e94d448142cea4ddc64b67.mailgun.org';
 
-const mg = mailgun({apiKey: process.env.API_KEY, domain: DOMAIN});
+const DB_NAME = "clubs_testing"; // @note - later change it according to database used in production
+
+const MONGO_DB_URI = `mongodb+srv://admin-vishal:${process.env.DB_PASSWORD}@cluster0-cujjf.mongodb.net/ClubsDB`; // @note - Don't modify this, if it doesn't work for you please ask
+
+
+mongoose
+  .connect(MONGO_DB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    dbName: DB_NAME,
+    w: "majority",
+  })
+  .catch((err) => {
+    console.error(`Error in DB connection: mongo DB couldn't be reached`);
+    console.error(err);
+    exit(1);
+  });
+
+const db = mongoose.connection; //access to the pending connection
+db.on("error", (err) => {
+  console.log(`Error in DB connection`);
+  console.error(err);
+});
+db.once("open", () => {
+  console.log(`Connected to the database : ${DB_NAME}`);
+});
+
 app.set('view engine','ejs');
-app.use(express.static("public"));
-
-//mongoose.connect("mongodb+srv://admin-vishal:Vishal@2611@cluster0-cujjf.mongodb.net/blogDB",{useNewUrlParser :true});
-mongoose.connect('mongodb://localhost/ClubsDB',{useNewUrlParser :true,useUnifiedTopology: true});
-
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-}))
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-
-///home route
-let Event = "";
+app.use(express.static("Public"));
 
 const clubsSchema =  new Schema({
   name:{
@@ -53,81 +61,72 @@ const clubsSchema =  new Schema({
 const Club = mongoose.model('club',clubsSchema);
 
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+let Event = "";
+
+app.get('/',function(req,res){
+  Club.find({},function(err,foundEvent){
+    if (err){
+      console.log(err);
+    }
+    else{
+      res.render("home",{Event :foundEvent});
+    }
+  })
+})
 
 
 
-app.post('/signup', (req, res, next) => {
+app.post('/signup', function(req, res){
 
-  User.findOne({'username': req.body.fname},function(err, user) {
-        if (err){
-          console.log('Error in SignUp: '+err);
-          return done(err);
-        }
-        if (user) {
-          console.log('User already exists');
-          return done(null, false,
-             req.flash('message','User Already Exists'));
+    console.log(req.body.password);
+      bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+      const user = new User({
+        username : req.body.fname,
+        email :req.body.email,
+        password : hash,
+      });
+      user.save(function(err){
+        if(err){
+          console.log(err);
         }
         else{
-          console.log("Hello");
-
-                const name = req.body.fname;
-                const email = req.body.email;
-                const password = req.body.password;
-
-                var token = jwt.sign({name,email,password},process.env.JWT_ACCOUNT_ACTIVATE,{expiresIn:'20m'})
-                const data = {
-          	     from:'noreply@nitp.ac.in',
-          	     to:req.body.email,
-          	     subject: 'Account Activation link',
-          	     html:`<p>please click on the link</p>
-                 Link to <a href="http://localhost:5000/${token}">${token}</a>`
-                };
-                mg.messages().send(data, function (error, body) {
-                  if(error){
-                    console.log(error);
-
-                  }else{
-                    return res.json({message:'Email has been send Please Activate your account'});
-                  }
-
-                });
-
-            }
+          Club.find({"name":req.body.fname},function(err,foundEvent){
+              if (err){
+                  console.log(err);
+              }
+              else{
+                  console.log(foundEvent);
+                  res.render("index",{Event :foundEvent,Name : req.body.fname});
+              }
           })
-
+        }
     })
+  })
+})
 
+app.post('/login',(req,res,next)=>{
+    username = req.body.fname,
+    password = req.body.password,
 
-app.get('/:token',(req,res,next)=>{
-  const token = req.params.token;
-
-  if(token){
-    jwt.verify(token,process.env.JWT_ACCOUNT_ACTIVATE,function(err,decodedToken){
-      if(err){
-        return res.status(400).json({error:"Incorrect or Expired link"});
+    User.find({"username": username},function(err,foundUser){
+      if (err){
+        return res.json({message:'Incorrect Email and Password'});
+        console.log(err);
       }
       else{
-
-        const {name,email,password} = decodedToken;
-
-        User.register(
-        {
-          username: name,
-          email: email,
-        },
-        password, (err, user) => {
-        if(err) {
-          console.log(err);
-          res.statusCode = 500;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({err: err});
-        }
-        else {
-          console.log("jgufuyf");
-            passport.authenticate('local')(req, res, () => {
+          bcrypt.compare(password,foundUser[0].password,function(err,result){
+            if(result === true){
+              Club.find({"name":req.body.fname},function(err,foundEvent){
+                if (err){
+                  console.log(err);
+                  res.render("home",{Event :foundEvent});
+                }
+                else{
+                  res.render("index",{Event :foundEvent,Name : req.body.fname});
+                }
+              })
+            }
+            else{
               Club.find({},function(err,foundEvent){
                 if (err){
                   console.log(err);
@@ -137,126 +136,13 @@ app.get('/:token',(req,res,next)=>{
                   res.render("home",{Event :foundEvent});
                 }
               })
-            // res.statusCode = 200;
-            // res.setHeader('Content-Type', 'application/json');
-            // res.json({success: true, status: 'Registration Successful!'});
-        })
-      }
-        });
-      }
-
-    })
-  }else{
-    return res.json({error:"Something went wrong"});
-  }
-
-})
-
-
-
-
-app.post('/login',(req,res,next)=>{
-  const user = new User({
-    username : req.body.fname,
-    password : req.body.password,
-  });
-
-  req.login(user,function(err){
-    if(err){
-      console.log(err);
-    }
-    else{
-      passport.authenticate('local')(req, res, () => {
-
-        console.log(req.body);
-
-
-        if(req.isAuthenticated()){
-          Club.find({"name":req.body.fname},function(err,foundEvent){
-            if (err){
-              console.log(err);
-            }
-            else{
-              console.log(foundEvent);
-              res.render("index",{Event :foundEvent,Name : req.body.fname});
             }
           })
-        }
-        else{
-          Club.find({},function(err,foundEvent){
-            if (err){
-              console.log(err);
-            }
-            else{
-              console.log(foundEvent);
-              res.render("home",{Event :foundEvent});
-            }
-          })
-        }
-        // res.statusCode = 200;
-        // res.setHeader('Content-Type', 'application/json');
-        // res.json({success: true, status: 'login Successful!'});
-      });
-    }
-  })
+      }
+    });
 })
 
-
-app.get('/',function(req,res){
-  Club.find({},function(err,foundEvent){
-    if (err){
-      console.log(err);
-    }
-    else{
-      console.log(foundEvent);
-      res.render("home",{Event :foundEvent});
-    }
-  })
-})
-
-
-app.post("/delete",function(req,res){
-
-  var ClubName = "";
-  console.log(req.body.checkbox);
-
-  Club.find({"_id":req.body.checkbox}, function (err, foundItem) {
-    if (err) {
-        console.log(err);
-    }else{
-      console.log(foundItem);
-      ClubName = foundItem[0].name;
-      console.log(ClubName);
-      Club.remove({"_id": foundItem._id});
-    }
-  })
-
-console.log(ClubName);
-
-  Club.find({name : ClubName},function(err,foundEvent){
-  if (err){
-    console.log(err);
-  }
-  else{
-    console.log(foundEvent);
-    res.render("index",{Event :foundEvent,Name :ClubName});
-  }
-  })
-
-})
-
-// app.get('/results',function(req,res){
-//   Club.find({},function(err,foundEvent){
-//     if (err){
-//       console.log(err);
-//     }
-//     else{
-//       console.log(foundEvent);
-//       res.render("index",{Event :foundEvent});
-//     }
-//   })
-// })
-
+//
 app.post('/',function(req,res){
 
   console.log(req.body);
@@ -267,103 +153,26 @@ app.post('/',function(req,res){
   });
 
   clubinfo.save();
+  //db.close();
   Club.find({name : req.body.ClubName},function(err,foundEvent){
     if (err){
       console.log(err);
     }
     else{
-      console.log(foundEvent);
       res.render("index",{Event :foundEvent,Name : req.body.ClubName});
     }
   })
-  //res.redirect('/results');
 })
+
+app.post("/delete",function(req,res){
+
+  Club.deleteOne({"_id": req.body.checkbox}, function (err) {
+    if(err) console.log(err);
+    console.log("Successful deletion");
+  });
+})
+
 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server listening on ${PORT}`));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const app = express();
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-//
-//
-// mongoose.connect('mongodb://localhost/ClubsDB',{useNewUrlParser :true,useUnifiedTopology: true});
-//
-// let Event = "";
-//
-// const clubsSchema =  new Schema({
-//   name:{
-//         type:String,
-//   },
-//   event:{
-//         type:String,
-//   },
-//   date:{
-//         type:String,
-//   }
-// });
-// const Club = mongoose.model('club',clubsSchema);
-//
-//
-// app.set('view engine','ejs');
-//
-// app.use(bodyParser.urlencoded({extended:true}));
-// app.use(express.static("public"));
-//
-//
-// app.get('/',function(req,res){
-//   res.render("home");
-// })
-//
-// app.get('/results',function(req,res){
-//   Club.find({},function(err,foundEvent){
-//     if (err){
-//       console.log(err);
-//     }
-//     else{
-//       console.log(foundEvent);
-//       res.render("results",{Event :foundEvent});
-//     }
-//   })
-// })
-//
-// app.post('/',function(req,res){
-//   const clubinfo = new Club({
-//     name: req.body.ClubName,
-//     event: req.body.event,
-//     date: req.body.Date,
-//   });
-//
-//   clubinfo.save();
-//   res.redirect('/results');
-// })
-//
-//
-//
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 5000;
-// }
-// app.listen(port,function(){
-//   console.log("server is running on 5000 port");
-// })
--- models/userSignup.js
+++ models/userSignup.js
@@ -1,19 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
+//const passportLocalMongoose = require("passport-local-mongoose");
 
 
 const UserSchema = new Schema({
 
     username:{
         type:String,
+        required:true,
     },
     email:{
-        type:String,
+      type: "string",
+      pattern: "^(\\([0-9]{3}\\))?[0-9]{3}-[0-9]{4}$"
     },
+    password:{
+      type:String,
+      required:true,
+    }
 
 })
-UserSchema.plugin(passportLocalMongoose,{ usernameField : 'username' });
+//UserSchema.plugin(passportLocalMongoose,{ usernameField : 'username' });
 
 const User = mongoose.model('user', UserSchema);
 
